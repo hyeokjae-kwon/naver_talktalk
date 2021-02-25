40c71e48af5ee01059c751de5da06bcee4021cb5
it0900 <it0900@gsitm.com>
2021-02-25 18:40:51 +0900

    db info clear

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,21 +1,4 @@
 /* DB Info */
 module.exports = (function() {
-    /*
-    return {
-        host : 'localhost'
-        , port : '3306'
-        , user : 'node_user'
-        , password : 'nodeuser1!'
-        , database : 'nodetest'
-    }
-    */
-    return { 
-        host: "127.0.0.1", 
-        port : 3307, 
-        user: "gbmp_user", 
-        password: "gbmp1234!@", 
-        database: "gspb"
-        //,connectionLimit: 5
-        //gsitm-bizmsg-dev-postgre-master.cricf5agbwwl.ap-northeast-2.rds.amazonaws.com
-    } 
+    
 })();
